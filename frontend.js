09450877e0ef18b76b1eff2eb5f29a3bdac7ba71
HADINAJIISTHEBEDT <hadinajiisthebedt@users.noreply.github.com>
2026-05-09 22:36:07 +0300

    Update categories: vegetables, groceries, fruits, desserts, dairy

--- frontend.js
+++ frontend.js
@@ -165,15 +165,10 @@ const I18N = {
     addedToCart: "Eklendi!",
     backToSearch: "Aramaya don",
     featuredFreshVegetables: "Taze sebzeler",
-    featuredBreadAndBakery: "Ekmek ve fırın ürünleri",
-    featuredDailyGroceries: "Günlük temel gıdalar",
-    featuredFruit: "Meyve",
-    featuredRiceAndGrains: "Pirinç ve tahıllar",
+    featuredDailyGroceries: "Günlük gıda ürünleri",
+    featuredFruits: "Meyveler",
     featuredDesserts: "Tatlılar",
-    featuredPotatoes: "Patates",
     featuredDairy: "Süt ürünleri",
-    featuredVegetables: "Sebzeler",
-    featuredBananas: "Muz",
   },
   en: {
     title: "PazarQuery",
@@ -217,15 +212,10 @@ const I18N = {
     addedToCart: "Added!",
     backToSearch: "Back to search",
     featuredFreshVegetables: "Fresh vegetables",
-    featuredBreadAndBakery: "Bread and bakery",
     featuredDailyGroceries: "Daily groceries",
-    featuredFruit: "Fruit",
-    featuredRiceAndGrains: "Rice and grains",
+    featuredFruits: "Fruits",
     featuredDesserts: "Desserts",
-    featuredPotatoes: "Potatoes",
     featuredDairy: "Dairy",
-    featuredVegetables: "Vegetables",
-    featuredBananas: "Bananas",
   },
   ar: {
     title: "PazarQuery",
@@ -269,15 +259,10 @@ const I18N = {
     backToSearch: "\u0627\u0644\u0639\u0648\u062f\u0629 \u0625\u0644\u0649 \u0627\u0644\u0628\u062d\u062b",
     addedToCart: "\u062a\u0645\u062a \u0627\u0644\u0625\u0636\u0627\u0641\u0629!",
     featuredFreshVegetables: "\u062e\u0636\u0631\u0648\u0627\u062a \u0637\u0627\u0632\u062c\u0629",
-    featuredBreadAndBakery: "\u0627\u0644\u062e\u0628\u0632 \u0648\u0627\u0644\u0645\u062e\u0628\u0632\u0627\u062a",
     featuredDailyGroceries: "\u0627\u0644\u0628\u0642\u0627\u0644\u0629 \u0627\u0644\u064a\u0648\u0645\u064a\u0629",
-    featuredFruit: "\u0627\u0644\u0641\u0627\u0643\u0647\u0629",
-    featuredRiceAndGrains: "\u0627\u0644\u0623\u0631\u0632 \u0648\u0627\u0644\u062d\u0628\u0648\u0628",
+    featuredFruits: "\u0627\u0644\u0641\u0627\u0643\u0647\u0629",
     featuredDesserts: "\u0627\u0644\u062d\u0644\u0648\u064a\u0627\u062a",
-    featuredPotatoes: "\u0627\u0644\u0628\u0637\u0637\u0633",
     featuredDairy: "\u0645\u0646\u062a\u062c\u0627\u062a \u0627\u0644\u0623\u0644\u0628\u0627\u0646",
-    featuredVegetables: "\u0627\u0644\u062e\u0636\u0631\u0648\u0627\u062a",
-    featuredBananas: "\u0627\u0644\u0645\u0648\u0632",
   },
 };
 
@@ -409,15 +394,10 @@ function applyLanguage() {
   const featuredItems = document.querySelectorAll(".ad-tile span");
   const featuredKeys = [
     "featuredFreshVegetables",
-    "featuredBreadAndBakery",
     "featuredDailyGroceries",
-    "featuredFruit",
-    "featuredRiceAndGrains",
+    "featuredFruits",
     "featuredDesserts",
-    "featuredPotatoes",
     "featuredDairy",
-    "featuredVegetables",
-    "featuredBananas",
   ];
   featuredItems.forEach((span, index) => {
     if (span && featuredKeys[index]) {
